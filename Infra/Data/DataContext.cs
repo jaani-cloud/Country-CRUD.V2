c5717feaf9dda4f9a79115cbb42f6da705a78fe0
heredikshit-ux <heredikshit@gmail.com>
2026-03-03 08:24:46 +0530

    conect to database nd add migration

--- Infra/Data/DataContext.cs
+++ Infra/Data/DataContext.cs
@@ -7,5 +7,5 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-    public DbSet<Country> countries { get; set; }
+    public DbSet<Country> Countries { get; set; }
 }
