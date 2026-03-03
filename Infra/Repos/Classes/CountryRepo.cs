bcb09c8a38490e1f4f43c1b975624186b2f1cdb0
heredikshit-ux <heredikshit@gmail.com>
2026-03-03 08:48:16 +0530

    Infra Repos

---
+++ Infra/Repos/Classes/CountryRepo.cs
@@ -0,0 +1,66 @@
+﻿using Domain.Entities;
+using Infra.Data;
+using Infra.Repos.Interfaces;
+
+namespace Infra.Repos.Classes;
+
+public class CountryRepo : ICountryRepo
+{
+    private readonly DataContext _context;
+
+    public CountryRepo(DataContext context)
+    {
+        _context = context;
+    }
+
+    public Country Add(Country country)
+    {
+        _context.Countries.Add(country);
+        _context.SaveChanges();
+
+        return country;
+    }
+
+    public void Delete(int id)
+    {
+        var country = _context.Countries.Find(id);
+
+        if (country == null)
+        {
+            return;
+        }
+        _context.Countries.Remove(country);
+        _context.SaveChanges();
+    }
+
+    public List<Country> GetAll()
+    {
+        return _context.Countries.ToList();
+    }
+
+    public Country? GetById(int id)
+    {
+        var country = _context.Countries.Find(id);
+
+        return country;
+    }
+
+    public Country? GetByName(string name)
+    {
+        var country = _context.Countries.FirstOrDefault(country => name.ToLower() == country.Name.ToLower());
+
+        return country;
+    }
+
+    public void Update(Country country)
+    {
+        var countryToUpdate = _context.Countries.Find(country.Id);
+
+        if (countryToUpdate == null)
+        {
+            return;
+        }
+        countryToUpdate.Name = country.Name;
+        _context.SaveChanges();
+    }
+}
---
+++ Infra/Repos/Interfaces/ICountryRepo.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Infra.Repos.Interfaces;
+
+public interface ICountryRepo
+{
+    public Country Add(Country country);
+    public List<Country> GetAll();
+    public Country? GetById(int id);
+    public void Update(Country country);
+    public void Delete(int id);
+    public Country? GetByName(string name);
+}
