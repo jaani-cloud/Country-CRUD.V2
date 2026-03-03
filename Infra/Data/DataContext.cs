49468c0f11cea4c1526e42c8b72ff7cc623f99ba
heredikshit-ux <heredikshit@gmail.com>
2026-03-03 07:45:12 +0530

    Ifra DataContext nd installed ef package to API nd Ifra

---
+++ Infra/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<Country> countries { get; set; }
+}
